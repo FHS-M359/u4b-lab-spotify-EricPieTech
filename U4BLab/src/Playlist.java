f766d6df866b2080e99dba58cc6ae1a0571ad8aa
EricPieTech <eric.pajtek@gmail.com>
2026-02-12 10:10:29 -0600

    Worked on Song & Playlist

--- U4BLab/src/Playlist.java
+++ U4BLab/src/Playlist.java
@@ -1,2 +1,17 @@
-public class Playlist {
+import java.io.*;
+import java.util.*;
+public class Playlist{
+    static ArrayList<Song> songs = new ArrayList<>();
+    public static void readSongs() throws FileNotFoundException{
+        Scanner inFile = new Scanner(new File("spotify_unique_years_artists.txt"));
+        while(inFile.hasNextLine()){
+            String[] temp = inFile.nextLine().split(",");
+            Song tempSong = new Song(temp[0], temp[1], temp[2], Integer.parseInt(temp[3]), Integer.parseInt(temp[4]), temp[5]);
+            songs.add(tempSong);
+        }
+        inFile.close();
+    }
+    public void printList(ArrayList<Song> input){
+
+    }
 }
--- U4BLab/src/Song.java
+++ U4BLab/src/Song.java
@@ -1,2 +1,15 @@
 public class Song {
+    private String title, artist, album, genre;
+    private int year, duration;
+    public Song(String title, String artist, String album, int duration, int year, String genre){
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.duration = duration;
+        this.year = year;
+        this.genre = genre;
+    }
+    public String toString(){
+        return(String.format("%-25s %-25s %-30s %-5d %-10s", title, artist, album, year, genre));
+    }
 }
